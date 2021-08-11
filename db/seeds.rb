ef3defa09f9d1cea21a1030e5a68cd98d3556a53
Marie Hasselberg <m.hasselberg@posteo.de>
2021-08-11 15:29:40 +0200

    Challenge 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "dolores",address: "mitte",category: "italian")
+Restaurant.create(name: "Mcd",address: "pberg",category: "chinese")
+Restaurant.create(name: "kebabshop",address: "wedding",category: "japanese")
+Restaurant.create(name: "santa maria",address: "kreuzberg",category: "french")
+Restaurant.create(name: "stranero",address: "berlin",category: "belgian")
